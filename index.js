d791e381f426bcfeb0fb3f312d8b009f856e31e7
MEAN Developer <jitendra@rapifly.com>
2024-06-22 11:03:41 +0530

    port

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import express from 'express';
 import mongoose from 'mongoose';
 import routes from './src/routes/todo.js';
 
-const port = 1212;
+const port = process.env.PORT || 1212;
 const app = express();
 
 mongoose.connect("mongodb+srv://mansimar:mansimar.todo123@todocluster.udfowtl.mongodb.net/TodoApp?retryWrites=true&w=majority&appName=ToDoCluster")
     .then(() => {
         console.log("Connected to DB!");
         app.listen(port, () => {
-            console.log("Server Started at-", port);
+            console.log("ToDo App Server Started at-", port);
         });
     }).catch((err) => {
         console.log("Error connecting to DB- ", err);
